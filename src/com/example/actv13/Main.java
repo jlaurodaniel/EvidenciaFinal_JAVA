7b908179cb58c32ce7121d8d6e6ef29ed43d8819
jlaurodaniel <jlaurodaniel@gmail.com>
2021-05-27 15:52:22 -0500

    Menu y login de administradores creado

--- src/com/example/actv13/Main.java
+++ src/com/example/actv13/Main.java
@@ -1,13 +1,18 @@
 package com.example.actv13;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.UUID;
 
 public class Main {
+    private String path;
 public static Scanner sc=new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("--------------------------------------------");
-        System.out.println("- Bienvenido a la clinica 'El Tecmi'");
+        System.out.println("- Bienvenido ADMIN a la clinica 'El Tecmi'");
         System.out.println("--------------------------------------------");
         System.out.println("- Escribe tu nombre de usuario: ");
         String user=sc.nextLine();
@@ -17,7 +22,69 @@ public static Scanner sc=new Scanner(System.in);
     }
     //Paciente paciente=new Paciente();
 
-    public  void Login(String pass, String user){
+    public  void Login(String pass, String user) throws IOException, ClassNotFoundException {
+        Administrador UsuarioSession;
+        boolean valid=false;
+        boolean menu=true;
 
+        ArrayList<Administrador> administradors= new ArrayList<Administrador>();
+        ObjectInputStream leyendoFichero = new ObjectInputStream( new FileInputStream(path) );
+        administradors = ( ArrayList <Administrador> )leyendoFichero.readObject();
+        leyendoFichero.close();
+
+        for (int i=0;i<administradors.size();i++){
+            if (administradors.get(i).nombre.equals(user) & administradors.get(i).password.equals(pass)){
+                 UsuarioSession=administradors.get(i);
+                 valid=true;
+            }
+        }
+
+        if (valid){
+            System.out.println("---- Usuario y contrase;a correctos  ------");
+            while (menu){
+                System.out.println("Escribe 1 para registrar un doctor" +
+                        "Escribe 2 para registrar paciente\n" +
+                        "Escribe 3 para registrar una cita\n" +
+                        "Escribe 4 para eliminar una cita\n" +
+                        "Escribe 5 para cerrar el programa");
+
+                int answer=sc.nextInt();
+                switch (answer){
+                    case 1:
+                        System.out.println("| * -------------------------------------------- * |");
+                        System.out.println("| *     ********  Nuevo Paciente  *********      * |");
+                        System.out.println("| * -------------------------------------------- * |");
+                        System.out.println("| *     Escribe tu nombre o usuario :            * |");
+                        String nombre=sc.nextLine();
+                        System.out.println("| *  Escribe la contraseña para "+nombre+" :     * |");
+                        String password=sc.nextLine();
+                        System.out.println("| *  Que padecimiento presenta actualmente?:     * |");
+                        String padecimiento=sc.nextLine();
+                        System.out.println("| *--------------------------------------------  * |");
+
+                        UUID uuid = UUID.randomUUID();
+                        String id = uuid.toString();
+
+                        Paciente paciente=new Paciente(id,nombre,password,padecimiento);
+                        if (paciente.altaPaciente()){
+                            System.out.println("*****  Paciente registrado satisfactoriamente   ****");
+                        }else {
+                            System.out.println("Ocurrio un problema al registrar el paciente, intente de nuevo");
+                        }
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        menu=false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
     }
 }
--- src/com/example/actv13/Paciente.java
+++ src/com/example/actv13/Paciente.java
@@ -8,9 +8,9 @@ import java.util.UUID;
 public class Paciente extends Usuario {
     public String padecimiento;
     private final String path="C:\\Users\\lauro\\OneDrive - Universidad Tecmilenio\\tarea\\TECMILENIO\\6to SEMESTRE\\Computacion en Java\\EvidenciaFinal\\src\\db\\paciente.txt";
-    private Scanner sc= new Scanner(System.in);
+    private final Scanner sc= new Scanner(System.in);
 
-    private Paciente(String id, String nombre, String password, String padecimiento) {
+    public Paciente(String id, String nombre, String password, String padecimiento) {
         super(id, nombre, password);
         this.padecimiento = padecimiento;
     }
@@ -18,16 +18,13 @@ public class Paciente extends Usuario {
 
     public Boolean altaPaciente() throws IOException, ClassNotFoundException {
 
-        ArrayList<Paciente> pacientes= new ArrayList<Paciente>();
+        ArrayList<Paciente> pacientes;
         ObjectInputStream leyendoFichero = new ObjectInputStream( new FileInputStream(this.path) );
         pacientes = ( ArrayList <Paciente> )leyendoFichero.readObject();
         leyendoFichero.close();
 
-        UUID uuid = UUID.randomUUID();
-        String id = uuid.toString();
-        Paciente paciente=new Paciente(this.id,this.nombre,this.password,this.padecimiento);
 
-        if (pacientes.add(paciente)){
+        if (pacientes.add(new Paciente(this.id,this.nombre,this.password,this.padecimiento))){
             ObjectOutputStream escribiendoFichero = new ObjectOutputStream(new FileOutputStream(this.path) );
             escribiendoFichero.writeObject(pacientes);
             escribiendoFichero.close();
