0956daf2fde56c9e8a3e6470217bb586ca8df7b7
jlaurodaniel <jlaurodaniel@gmail.com>
2021-05-27 16:14:16 -0500

    Metodo alta de doctores

--- src/com/example/actv13/Doctor.java
+++ src/com/example/actv13/Doctor.java
@@ -1,12 +1,32 @@
 package com.example.actv13;
 
-public class Doctor extends  Usuario{
-    public Usuario usuario;
-    public String especialidad;
+import java.io.*;
+import java.util.ArrayList;
 
-    public Doctor(String id, String nombre, String password, Usuario usuario, String especialidad) {
+public class Doctor extends  Usuario{
+    public String especialidad;
+    private final String path="C:\\Users\\lauro\\OneDrive - Universidad Tecmilenio\\tarea\\TECMILENIO\\6to SEMESTRE\\Computacion en Java\\EvidenciaFinal\\src\\db\\doctor.txt";
+
+    public Doctor(String id, String nombre, String password, String especialidad) {
         super(id, nombre, password);
-        this.usuario = usuario;
         this.especialidad = especialidad;
+    }
+
+    public Boolean altaDoctor() throws IOException, ClassNotFoundException {
+
+        ArrayList<Doctor> doctors;
+        ObjectInputStream leyendoFichero = new ObjectInputStream( new FileInputStream(this.path) );
+        doctors = ( ArrayList <Doctor> )leyendoFichero.readObject();
+        leyendoFichero.close();
+
+
+        if (doctors.add(new Doctor(this.id,this.nombre,this.password,this.especialidad))){
+            ObjectOutputStream escribiendoFichero = new ObjectOutputStream(new FileOutputStream(this.path) );
+            escribiendoFichero.writeObject(doctors);
+            escribiendoFichero.close();
+            return true;
+        }
+
+        return false;
     }
 }
